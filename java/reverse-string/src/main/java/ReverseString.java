5179064d114bb2a94b9bd183782b6c6bd7451985
souvikmaji <souvikmaji94@gmail.com>
2019-09-22 11:11:00 +0530

    reverse string implemented with string builder

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,17 +1,7 @@
 class ReverseString {
 
 	String reverse(String inputString) {
-
-		char[] inputStringArray = inputString.toCharArray();
-
-		int n = inputStringArray.length;
-		for (int i = 0; i < n / 2; i++) {
-			char temp = inputStringArray[i];
-			inputStringArray[i] = inputStringArray[n - i - 1];
-			inputStringArray[n - i - 1] = temp;
-		}
-
-		return new String(inputStringArray);
+		return new StringBuilder(inputString).reverse().toString();
 	}
 
 }
