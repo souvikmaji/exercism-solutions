6f84c6cd6f32122e626365538d6430b2d1ea8a29
souvikmaji <souvikmaji94@gmail.com>
2019-11-11 11:44:41 +0530

    isogram solved

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,7 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class IsogramChecker {
 
-    boolean isIsogram(String phrase) {
-       return true;
-    }
+	boolean isIsogram(String phrase) {
+		Map<Character, Integer> countMap = new HashMap<>();
+
+		for (char ch : phrase.toCharArray()) {
+			char lowerCaseChar = Character.toLowerCase(ch);
+			if (Character.isAlphabetic(lowerCaseChar)) {
+				if (countMap.containsKey(lowerCaseChar)) {
+					return false;
+				}
+				countMap.put(lowerCaseChar, 0);
+			}
+
+		}
+		return true;
+	}
 
 }
