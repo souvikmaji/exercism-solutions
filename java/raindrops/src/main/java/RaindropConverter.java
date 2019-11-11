6b8e27d65a2df150ad0183f950f103f02b0a164a
souvikmaji <souvikmaji94@gmail.com>
2019-11-11 11:31:01 +0530

    raindrop solved

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,41 @@
+import java.util.Optional;
+
 class RaindropConverter {
 
-    String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	private static final String PLONG = "Plong";
+	private static final String PLANG = "Plang";
+	private static final String PLING = "Pling";
+
+	private boolean isDivisible(int number, int divisor) {
+		return number % divisor == 0;
+	}
+
+	private Optional<String> getDivisorString(int number) {
+		StringBuilder s = new StringBuilder();
+
+		if (isDivisible(number, 3)) {
+			s.append(PLING);
+		}
+		
+		if (isDivisible(number, 5)) {
+			s.append(PLANG);
+		}
+		
+		if (isDivisible(number, 7)) {
+			s.append(PLONG);
+		}
+
+		if (s.length() > 0) {
+			return Optional.of(s.toString());
+		}
+		
+		return Optional.empty();
+	}
+	
+	String convert(int number) {
+		return getDivisorString(number).orElse(String.valueOf(number));
+	}
+
+	
 
 }
