39cf57365393f4d924b75a411a87182fc015b479
souvikmaji <souvikmaji94@gmail.com>
2019-11-13 08:34:08 +0530

    luhn solved

--- java/luhn/src/main/java/LuhnValidator.java
+++ java/luhn/src/main/java/LuhnValidator.java
@@ -1,7 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class LuhnValidator {
 
-    boolean isValid(String candidate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	private int valueOf(int x, int position) {
+		if (position % 2 == 0) {
+			int doubleOf = x * 2;
+			if (doubleOf > 9) {
+				return doubleOf - 9;
+			}
+			return doubleOf;
+		}
+		return x;
+	}
+
+	boolean isValid(String candidate) {
+		String trimmed = candidate.replaceAll(" ", "");
+		if (trimmed.length() < 2) {
+			return false;
+		}
+		
+
+		List<Integer> candidateList = new ArrayList<>();
+		for (char c : trimmed.toCharArray()) {
+			if (!Character.isDigit(c)) {
+				return false;
+			}
+			candidateList.add(Character.digit(c, 10));
+		}
+
+		int sum = 0;
+		for (int i = 0; i < candidateList.size() ; i++) {
+			sum += valueOf(candidateList.get(candidateList.size() - i - 1), i+1);
+			
+		}
+
+		return sum % 10 == 0;
+	}
 
 }
